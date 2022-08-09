e15e4f9566b6395929ed0961ccdaf50ebe6d9535
Shannon Bonet <shannonbonet@berkeley.edu>
2022-08-09 15:31:55 -0400

    v1

--- diff.tsx
+++ diff.tsx
@@ -1,8 +1,35 @@
-
-
-
+const [hello, setHello] = React.useState('')
+const [goodbye, setGoodbye] = React.useState('')
 const Hello = () => {
     return (
-        <div> </div>
+        <div>
+            shanhello
+        </div>
     )
 }
+
+const [state, setState] = React.useState(null)
+
+const OneLines = () => {
+    return (
+        <div>
+            hi bonet
+        </div>
+    )
+} 
+
+const TWOLines = () => {
+    return (
+        <div>
+            hello
+        </div>
+    )
+} 
+
+const MOREMOREMROELines = () => {
+    return (
+        <div>
+            hello
+        </div>
+    )
+} 
